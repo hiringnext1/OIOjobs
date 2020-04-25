e74e738ec01d2c9f3fdc78689a036b61e1fff1a3
hiringnext1 <hiringnext1@gmail.com>
2020-04-25 16:31:02 +0530

    update

--- scrapydjango/settings.py
+++ scrapydjango/settings.py
@@ -68,7 +68,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-
 ROOT_URLCONF = 'scrapydjango.urls'
 
 TEMPLATES = [
@@ -159,10 +158,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root', )
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
-
 import dj_database_url
 
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
-
